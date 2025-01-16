b60770c25aae1e9c34d55acd5d8b8a9cfb3212a8
portoduque <portoduque@outlook.com>
2025-01-15 20:16:04 -0400

    Add project files.

---
+++ MeuDiarioApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MeuDiarioApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
